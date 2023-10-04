5e94dc274c62a3725d88430b3442e7b3ae275266
Emmanuel Cobian Duarte <ecobian12310@gmail.com>
2023-10-04 12:48:58 -0700

    added ejs

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const fs = require("fs");
+const ejs = require('ejs');
 const path = require('path');
 const port = 3000;
 
@@ -10,19 +11,17 @@ let totalViews = 0;
 if (fs.existsSync("views.txt")) {
   totalViews = parseInt(fs.readFileSync("views.txt", "utf8"));
 }
+
 app.get("/", (req, res) => {
   totalViews++;
 
-  fs.writeFileSync("views.txt", "" + totalViews);
-
   let html = fs.readFileSync(__dirname + "/index.html", "utf8");
-  html = html.replace("{{views}}", totalViews);
+  html = ejs.render(html, {views: totalViews});
 
   res.send(html);
 });
 
 app.use(express.static(path.join(__dirname)));
-
 
 app.listen(port, () => {
   console.log(`Personal website being hosted at: http://localhost:${port}`);
